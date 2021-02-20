695b12e830036c974f5a101d20809a287e7e6bbf
Grandhi Akhil Sai Manoj <55430217+AkhilGrandhi@users.noreply.github.com>
2021-02-20 10:26:06 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,14 @@
 import firebase from "firebase"; 
 
   const firebaseApp = firebase.initializeApp({
-
-    apiKey: "AIzaSyBKOoiHjWp9Fm8Njc_pldQgujbWnXPMoTM",
-  authDomain: "insta-clone-b356d.firebaseapp.com",
-  databaseURL: "https://insta-clone-b356d.firebaseio.com",
-  projectId: "insta-clone-b356d",
-  storageBucket: "insta-clone-b356d.appspot.com",
-  messagingSenderId: "934981637038",
-  appId: "1:934981637038:web:7f54d71633473b6b366e3f",
-  measurementId: "G-H9LHNVSY7Y"
+    apiKey: "AIzaSyDdfL4z8RT2sFh1phK-xVIB96hYg4uXqho",
+authDomain: "insta-clone2-b9b5b.firebaseapp.com",
+databaseURL: "https://insta-clone2-b9b5b-default-rtdb.firebaseio.com",
+projectId: "insta-clone2-b9b5b",
+storageBucket: "insta-clone2-b9b5b.appspot.com",
+messagingSenderId: "354899919573",
+appId: "1:354899919573:web:48469eda8e1f9095fd985c",
+measurementId: "G-LKGG3J748J"
 
 })  
 
